274dc65e9d8a470c4d6d788dd6f8e7269dc5fe8f
SilviaOrgaz <silvia.og.2309@gmail.com>
2022-10-24 23:01:52 +0200

    estoy escribiendo comertarios

--- hash_2.py
+++ hash_2.py
@@ -1,15 +1,17 @@
 
 class hash_tabla:
-    #En primer lugar creo un constructor donde se crea la tabla hash.
-    def __init__(self):
-        self.tabla = [None] * 125
+    #En primer lugar creo un constructor donde se crea la tabla hash de un determinado tamaño: tamaño_tab 
+    def __init__(self, tamaño_tab):
+        self.tamaño_tab = tamaño_tab
+        self.tabla = [None] * tamaño_tab
 
-    #Funcion hash
-    def funcion_hash(self, value):
-        key = 0
-        for i in range(0, len(value)):
-            key +=ord(value[i])
-        return key % 125
+    #Calcula la función hash de una cadena.
+    #Recorro la cadena sumando los valores ascii de los elementos de la cadena.
+    def funcion_hash(self, cadena):
+        llave = 0
+        for i in range(0, len(cadena)):
+            llave +=ord(cadena[i])
+        return cadena % self.tamaño_tab
     
     #Método para ingresar elementos
     def Insertar_elementos(self, value):
